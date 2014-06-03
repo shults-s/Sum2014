29bd7a1afd6c1062da1ade67da07dc59e2dd4e01
unknown <SPR03@R111-24.(none)>
2014-06-03 13:07:38 +0400

    First project files

---
+++ T00FIRST/T00FIRST.c
@@ -0,0 +1,14 @@
+ /* SS3 02.06.2014 */
+
+#include <stdio.h>
+#include <windows.h>
+
+ int main( int argc, char *argv[] )
+ {
+
+   printf("SS3\n");
+
+   MessageBox(NULL, "Text\nText2\nText3", "Title", MB_ICONHAND | MB_OK);
+
+   return 0;
+ }
