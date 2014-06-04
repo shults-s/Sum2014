bf52f2ab3fc3ab53272e43a32c898085dfadca7d
unknown <SPR03@R111-24.(none)>
2014-06-04 11:32:21 +0400

    New variant

--- T02CLOCK/T02CLOCK.c
+++ T02CLOCK/T02CLOCK.c
@@ -52,6 +52,27 @@ INT WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance,
   return msg.wParam;
 }
 
+VOID DrawArrow( HDC hDC, INT Xc, INT Yc, INT L, INT W, FLOAT Angle )
+{
+  INT i;
+  POINT pts[] =
+  {
+    {0, -W}, {-W, 0}, {0, L}, {W, 0}
+  }, pts_draw[sizeof pts / sizeof pts[0]];
+  FLOAT si = sin(Angle), co = cos(Angle);
+
+  for (i = 0; i < sizeof pts / sizeof pts[0]; i++)
+  {
+    pts_draw[i].x = Xc + (pts[i].x * co - pts[i].y * si);
+    pts_draw[i].y = Yc - (pts[i].x * si + pts[i].y * co);
+  }
+  SelectObject(hDC, GetStockObject(DC_BRUSH));
+  SelectObject(hDC, GetStockObject(DC_PEN));
+  SetDCPenColor(hDC, RGB(0, 0, 0));
+  SetDCBrushColor(hDC, RGB(255, 255, 255));
+  Polygon(hDC, pts_draw, sizeof pts / sizeof pts[0]);
+}
+
 LRESULT CALLBACK MyWindowFunc( HWND hWnd, UINT Msg,
                                WPARAM wParam, LPARAM lParam )
 {
@@ -63,11 +84,15 @@ LRESULT CALLBACK MyWindowFunc( HWND hWnd, UINT Msg,
   static INT cc = 0;
   static HBITMAP hBmClock, hBmFrame;
   static INT W, H;
+  SYSTEMTIME st;
+  FLOAT PI = 3.14159265358979323846;
+
+
 
   switch (Msg)
   {
   case WM_CREATE:
-    SetTimer(hWnd, 7, 30, NULL);
+    SetTimer(hWnd, 7, 10, NULL);
     hBmClock = LoadImage(NULL, "bg.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
     return 0;
 
@@ -104,10 +129,14 @@ LRESULT CALLBACK MyWindowFunc( HWND hWnd, UINT Msg,
     hMemDCClock = CreateCompatibleDC(hDC);
     SelectObject(hMemDCClock, hBmClock);
     GetObject(hBmClock, sizeof(bm), &bm);
-    BitBlt(hMemDC, 0, 0, bm.bmWidth, bm.bmHeight, hMemDCClock, 0, 0, SRCCOPY);
+    BitBlt(hMemDC, W / 2 - bm.bmWidth / 2, H / 2 - bm.bmHeight / 2, bm.bmWidth, bm.bmHeight, hMemDCClock, 0, 0, SRCCOPY);
 
-    MoveToEx(hMemDC, 175, 175, NULL);
-    LineTo(hMemDC, 270 - cc, 270);
+    GetLocalTime(&st);
+
+    DrawArrow(hMemDC, W / 2, H / 2, 100, 20, (-(st.wHour % 12 + st.wMinute / 60.0) / 12.0) * 2 * PI);
+    DrawArrow(hMemDC, W / 2, H / 2, 150, 15, (-(st.wMinute + st.wSecond / 60.0) / 60.0) * 2 * PI);
+    DrawArrow(hMemDC, W / 2, H / 2, 150, 3, (-(st.wSecond + st.wMilliseconds / 1000.0) / 60.0) * 2 * PI);
+
 
     BitBlt(hDC, 0, 0, W, H, hMemDC, 0, 0, SRCCOPY);
     DeleteDC(hMemDC);
